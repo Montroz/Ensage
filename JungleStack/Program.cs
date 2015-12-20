79f44a6fa7cea5f64990a1ba86ed288d672634d8
Montroz <Montroz@rambler.ru>
2015-12-20 03:10:56 +0300

    Update Program.cs

--- JungleStack/Program.cs
+++ JungleStack/Program.cs
@@ -22,7 +22,7 @@ namespace JungleStack
         {
             new Vector3(3402, -550, 127),  // Wait for attack
             new Vector3(4902, 450, 256), // Move for pulling
-            new Vector3(3402, -550, 127) // Wait position
+            new Vector3(3302, -550, 127) // Wait position
         };
 
         private const int StartTimeRadiant = 47;
